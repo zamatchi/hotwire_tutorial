67f8a36a1309f9b7f16d15c24197c67aa00a5c26
zamatchi <kazama199901@gmail.com>
2023-10-15 03:19:48 +0900

    Add: インラインバリデーションの実装

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def self.ransackable_attributes(auth_object = nil)
     ['name', 'age']
   end
